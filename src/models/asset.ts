1fb4bdd455ba75d3f20b26f51739441f75287d78
YardenShoham <hrsi88@gmail.com>
2019-12-04 21:58:36 +0200

    Add documentation

--- src/models/asset.ts
+++ src/models/asset.ts
@@ -1,6 +1,19 @@
+/**
+ * Represents a crypto asset.
+ *
+ * For example: BTC, ETH, USDT...
+ */
 export class Asset {
+  /**
+   * The name of the asset. Could be "USDT", "ETH", "BTC" etc...
+   */
   public name: string;
 
+  /**
+   * Constructs a new crypto asset.
+   *
+   * @param name The name of the asset. Could be "USDT", "ETH", "BTC" etc...
+   */
   constructor(name: string) {
     this.name = name;
   }
