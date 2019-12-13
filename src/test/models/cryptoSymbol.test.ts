8ada36aa966b3d11dec46d5b3e82af62d629ad19
YardenShoham <hrsi88@gmail.com>
2019-12-13 20:52:37 +0200

    Add CryptoSymbol test

---
+++ src/test/models/cryptoSymbol.test.ts
@@ -0,0 +1,30 @@
+import { CryptoSymbol } from "./../../models/cryptoSymbol";
+import { suite, describe, it } from "mocha";
+import { CryptoSymbolInfo } from "../../models/cryptoSymbolInfo";
+import { Asset } from "../../models/asset";
+import { Chance } from "../../models/chance";
+import * as config from "config";
+import { SymbolEvent } from "./../../models/symbolEvent";
+
+suite("CryptoSymbol", function(): void {
+  describe("event handling", function(): void {
+    it("should fire a SymbolEvent when Chance fires an event", function(done: Mocha.Done): void {
+      const chance = new Chance();
+      const cryptoSymbol = new CryptoSymbol(
+        new CryptoSymbolInfo(new Asset("ABC")),
+        chance
+      );
+      cryptoSymbol.on(
+        config.get("cryptoSymbolEventName"),
+        (symbolEvent: SymbolEvent): void => {
+          if (symbolEvent) {
+            done();
+          } else {
+            done("No SymbolEvent");
+          }
+        }
+      );
+      chance.emit(config.get("chanceEventName"));
+    });
+  });
+});
