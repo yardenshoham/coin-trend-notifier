5319cbff0a056dedfd1b7b9d098517fd794473d6
YardenShoham <hrsi88@gmail.com>
2019-12-13 22:15:36 +0200

    Fix wrong relative path in index test

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -3,7 +3,7 @@ import { suite, describe, it } from "mocha";
 suite("index", function(): void {
   describe("file", function(): void {
     it("should not crash", function(): void {
-      require("../../index");
+      require("../index");
     });
   });
 });
