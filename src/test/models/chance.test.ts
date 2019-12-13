4c2d5c3380f67ccca3ec40e89fa1b7571068dff6
YardenShoham <hrsi88@gmail.com>
2019-12-13 15:03:25 +0200

    Add a new addProbability test
    It should not emit an event when a new probability is given but a new threshold is not reached.

--- src/test/models/chance.test.ts
+++ src/test/models/chance.test.ts
@@ -2,7 +2,7 @@ import { Chance } from "./../../models/chance";
 import { suite, describe, it } from "mocha";
 import * as config from "config";
 
-suite("Chance suite", function(): void {
+suite("Chance", function(): void {
   describe("addProbability()", function(): void {
     it("should emit an event when a probability reaches a new threshold", function(done: Mocha.Done): void {
       this.timeout(100);
@@ -19,6 +19,16 @@ suite("Chance suite", function(): void {
       chance.on(config.get("chanceEventName"), () => {
         done("symbolEvent was thrown");
       });
+    });
+
+    it("should not emit an event when a new probability is given but a new threshold is not reached", function(done: Mocha.Done): void {
+      setTimeout(done, 20);
+      const chance = new Chance();
+      chance.addProbability(0.1);
+      chance.on(config.get("chanceEventName"), () => {
+        done("symbolEvent was thrown");
+      });
+      chance.addProbability(0.001);
     });
   });
 });
