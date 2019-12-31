a64deeb26a40b5866a5b9114ca647e68bcab1939
YardenShoham <hrsi88@gmail.com>
2019-12-31 23:29:28 +0200

    :fire: Remove middleware

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,8 @@
 import { createExpressServer } from "routing-controllers";
-import express from "express";
 
 /**
  * The express app that'll represent the api server.
  */
-const app = createExpressServer({
-  middlewares: [express.json(), express.urlencoded({ extended: true })]
-});
+const app = createExpressServer({});
 
 export default app;
