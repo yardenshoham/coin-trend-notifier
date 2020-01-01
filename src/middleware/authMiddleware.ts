d8e5e75581a78e801e540b62f594529f49806afd
YardenShoham <hrsi88@gmail.com>
2020-01-01 17:01:32 +0200

    :sparkles: Add AuthMiddleware

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,49 @@
+import { ExpressMiddlewareInterface } from "routing-controllers";
+import { Response, NextFunction } from "express";
+import UserJwtPayload from "./../interfaces/userJwtPayload";
+import AuthorizedRequest from "../interfaces/authorizedRequest";
+import jwt from "jsonwebtoken";
+import config from "config";
+import { UNAUTHORIZED, BAD_REQUEST } from "http-status-codes";
+
+/**
+ * Decodes the jwt provided by the Authorization Bearer <token> header and assigns it to req.jwtPayload.
+ * If the header was not provided it returns an error with an UNAUTHORIZED status.
+ * If the header was provided but was not in the shape of Authorization Bearer <token> it returns an error with a BAD_REQUEST status.
+ * If the token was provided but was not valid it returns an error with a BAD_REQUEST status.
+ *
+ * An error is of type { error: string }
+ */
+export default class AuthMiddleware implements ExpressMiddlewareInterface {
+  /**
+   * The actual middleware function.
+   * @param req The request with an Authorization header.
+   * @param res The Express response object.
+   * @param next The Express next function.
+   */
+  use(req: AuthorizedRequest, res: Response, next?: NextFunction) {
+    if (req.headers.authorization) {
+      const parts = req.headers.authorization.split(" ");
+      if (parts.length === 2 && parts[0] === "Bearer") {
+        try {
+          req.jwtPayload = jwt.verify(
+            parts[1],
+            config.get("jwtPrivateKey")
+          ) as UserJwtPayload;
+          next();
+        } catch (error) {
+          return res.status(BAD_REQUEST).send({ error: "Invalid token." });
+        }
+      } else {
+        return res.status(BAD_REQUEST).send({
+          error:
+            'Authorization header not complying with "Authorization Bearer <token>".'
+        });
+      }
+    } else {
+      return res
+        .status(UNAUTHORIZED)
+        .send({ error: "Access denied. No token provided." });
+    }
+  }
+}
---
+++ src/test/middleware/authMiddleware.test.ts
@@ -0,0 +1,91 @@
+import { suite, describe, it } from "mocha";
+import server from "./../../index";
+import { expect } from "chai";
+import { userDbPromise } from "./../../models/user";
+import request from "supertest";
+import { assetDbPromise } from "../../models/asset";
+import { cryptoSymbolDbPromise } from "../../models/cryptoSymbol";
+import UserDtoIn from "./../../interfaces/dtos/userDtoIn";
+import { UNAUTHORIZED, BAD_REQUEST } from "http-status-codes";
+
+const testRoute = "/api/preferences";
+suite("AuthMiddleware", function() {
+  describe("use()", function() {
+    it("should indicate success if a valid token was provided", async function() {
+      const user: UserDtoIn = {
+        email: "abc@def.com",
+        username: "atestuser",
+        password: "atestpassword"
+      };
+
+      // signup
+      await request(server)
+        .post("/api/users")
+        .send(user);
+
+      // login
+      let response = await request(server)
+        .post("/api/users/login")
+        .send({
+          email: user.email,
+          password: user.password
+        });
+
+      const { jwt } = response.body;
+
+      response = await request(server)
+        .post(testRoute)
+        .set("Authorization", `Bearer ${jwt}`)
+        .send({
+          baseAssetName: "BTC",
+          quoteAssetName: "USDT",
+          probability: 0.6
+        });
+
+      expect(response.statusType).to.equal(2);
+
+      // cleanup
+      (await cryptoSymbolDbPromise).c.deleteMany({});
+      (await assetDbPromise).c.deleteMany({});
+      (await userDbPromise).c.deleteMany({});
+    });
+
+    it("should return UNAUTHORIZED if Authorization header was not provided", async function() {
+      const response = await request(server)
+        .post(testRoute)
+        .send({
+          baseAssetName: "BTC",
+          quoteAssetName: "USDT",
+          probability: 0.6
+        });
+
+      expect(response.status).to.equal(UNAUTHORIZED);
+    });
+
+    it("should return BAD_REQUEST if Authorization header was not structured properly", async function() {
+      const response = await request(server)
+        .post(testRoute)
+        .set("Authorization", "I'm not structured properly")
+        .send({
+          baseAssetName: "BTC",
+          quoteAssetName: "USDT",
+          probability: 0.6
+        });
+
+      expect(response.status).to.equal(BAD_REQUEST);
+    });
+
+    it("should return BAD_REQUEST if an invalid token was provided", async function() {
+      const response = await request(server)
+        .post(testRoute)
+        .set("Authorization", "Bearer I'm-not-a-token")
+        .send({
+          baseAssetName: "BTC",
+          quoteAssetName: "USDT",
+          probability: 0.6
+        });
+
+      expect(response.status).to.equal(BAD_REQUEST);
+    });
+  });
+});
