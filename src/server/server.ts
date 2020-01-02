03ae92a3f5aab4c866745d8e6b3b200335049e8e
YardenShoham <hrsi88@gmail.com>
2020-01-02 19:58:01 +0200

    :green_heart: Remove logging during testing

--- src/server/server.ts
+++ src/server/server.ts
@@ -15,17 +15,23 @@ const app = express();
 app.use(helmet());
 app.use(compression());
 app.use(
-  morgan(function(tokens, req, res) {
-    return [
-      chalk.hex("#34ace0").bold(tokens.method(req, res)),
-      chalk.hex("#ff5252").bold(tokens.url(req, res)),
-      chalk.hex("#ffb142").bold(tokens.status(req, res)),
-      chalk.hex("#2ed573").bold(tokens["response-time"](req, res) + " ms"),
-      chalk.hex("#f78fb3").bold("@ " + tokens.date(req, res)),
-      chalk.yellow(tokens["remote-addr"](req, res)),
-      chalk.hex("#1e90ff")(tokens["user-agent"](req, res))
-    ].join(" ");
-  })
+  morgan(
+    function(tokens, req, res) {
+      return [
+        chalk.hex("#34ace0").bold(tokens.method(req, res)),
+        chalk.hex("#ff5252").bold(tokens.url(req, res)),
+        chalk.hex("#ffb142").bold(tokens.status(req, res)),
+        chalk.hex("#2ed573").bold(tokens["response-time"](req, res) + " ms"),
+        chalk.hex("#f78fb3").bold("@ " + tokens.date(req, res)),
+        chalk.yellow(tokens["remote-addr"](req, res)),
+        chalk.hex("#1e90ff")(tokens["user-agent"](req, res))
+      ].join(" ");
+    },
+    {
+      skip: req =>
+        req.headers["user-agent"].match(/node-superagent/).length != 0
+    }
+  )
 );
 
 useExpressServer(app, {
