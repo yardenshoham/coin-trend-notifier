5db88ae7e5b82b9dead5cded1138f052b14f1678
YardenShoham <hrsi88@gmail.com>
2020-01-04 12:48:11 +0200

    :sparkles: Add notifier

---
+++ src/interfaces/notifier.ts
@@ -0,0 +1,16 @@
+import { SymbolEvent } from "./../models/symbolEvent";
+/**
+ * An object with the ability to notify users of [[SymbolEvent]]s through various channels.
+ */
+export default interface Notifier {
+  /**
+   * Initializes this notifier.
+   */
+  start?(): void | Promise<void>;
+
+  /**
+   * Given a symbol event, notifies all its followers of the event.
+   * @param symbolEvent The event to notify about.
+   */
+  notify(symbolEvent: SymbolEvent): void | Promise<void>;
+}
